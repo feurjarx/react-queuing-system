cd37e5cc17fe078bb370130c281696d8eb2b1ac5
Roman <feurjarx@gmail.com>
2017-02-05 23:31:41 +0600

    TypeScript JSX  integrate

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import { Hello } from "./components/hello";
+
+export class App extends React.Component<any, undefined> {  
+    render() {
+         return <Hello />;
+    }
+}
---
+++ src/components/hello.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class Hello extends React.Component<any, undefined> {
+  render() {
+    return (
+      <h1>Test</h1>
+    )
+  }
+}
